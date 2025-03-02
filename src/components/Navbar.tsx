bf16b06df11b3c13d4ba7b5bfd10f8a873edd918
Rich Jones <103132290+richjones4364@users.noreply.github.com>
2025-03-02 21:46:33 +0000

    removed features and contact

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,18 +15,18 @@ const Navbar = () => {
               </a>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-              <a href="#features" className="border-transparent text-gray-700 hover:text-orange-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+              {/* <a href="#features" className="border-transparent text-gray-700 hover:text-orange-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 Features
-              </a>
+              </a> */}
               {/* <a href="#testimonials" className="border-transparent text-gray-700 hover:text-orange-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 Testimonials
               </a> */}
               <a href="/demo" className="border-transparent text-gray-700 hover:text-orange-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 Demo
               </a>
-              <a href="#contact" className="border-transparent text-gray-700 hover:text-orange-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+              {/* <a href="#contact" className="border-transparent text-gray-700 hover:text-orange-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 Contact
-              </a>
+              </a> */}
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
