f2595a07d9bf21c8dafbbdc0505d21317e73bed3
Rich Jones <103132290+richjones4364@users.noreply.github.com>
2025-03-02 18:52:13 +0000

    home page styling changes removed buttons

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -6,21 +6,16 @@ const CTASection = () => {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
           <span className="block">Ready to reduce workload?</span>
-          <span className="block text-orange-100">Book a demo today.</span>
+          <span className="block text-orange-100">Get In Touch Today!</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
-            <a
-              href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-orange-600 bg-white hover:bg-orange-50"
-            >
-              Book a Demo
-            </a>
+            
           </div>
           <div className="ml-3 inline-flex rounded-md shadow">
             <a
               href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-400 hover:bg-orange-700"
             >
               Contact Us
             </a>
