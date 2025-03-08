2b03913ac8a6848a02e365b5a3ead600fbf5f56c
Rich Jones <103132290+richjones4364@users.noreply.github.com>
2025-03-08 07:31:45 +0000

    lead capture added to demo page

--- nextjs/pages/DemoPage.tsx
+++ nextjs/pages/DemoPage.tsx
@@ -1,4 +1,5 @@
 import IncidentReport from "../components/IncidentReport";
+import LeadCaptureSection from "../components/LeadCaptureSection";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -6,11 +7,11 @@ const DemoPage = () => {
     return (
         <div>
             <Navbar />
-            <div className="bg-orange-100 py-12">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="bg-orange-50 rounded-lg shadow-lg p-8">
+            <div className="bg-orange-200 py-12">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-2">
+                    <div className="bg-orange-100 rounded-lg shadow-lg p-8">
                         <div className="text-center space-y-4"> {/* Add space to parent */}
-                            <h3 className="text-3xl font-bold text-gray-900">Try our Incident Report</h3> {/* Style h3 */}
+                            <h3 className="text-3xl font-bold text-gray-900">Meet Lucy, the Pastoral Team Assistant</h3> {/* Style h3 */}
                             <h4 className="text-xl font-medium text-gray-700">Help your Pastoral Team triage more efficiently.</h4> {/* Style h4 */}
                             <IncidentReport />
                             <h6 className="font-bold text-gray-800">Instructions:</h6> {/* Style h6 */}
@@ -19,9 +20,11 @@ const DemoPage = () => {
                                 <p>Explain what has happened to you and how it made you feel.</p>
                                 <p>The PDF download at the end shows how the incident report can be sent to the pastoral team for triage.</p>
                             </div>
+
                         </div>
                     </div>
                 </div>
+                <LeadCaptureSection />
             </div>
             <Footer />
         </div>
