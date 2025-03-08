311dd0adb33d2ca89de9155795caabeb4500b166
Rich Jones <103132290+richjones4364@users.noreply.github.com>
2025-03-08 18:34:41 +0000

    privacy page struxtured

--- nextjs/components/Footer.tsx
+++ nextjs/components/Footer.tsx
@@ -51,7 +51,8 @@ const Footer = () => {
           </a>
         </div>
         <a href="/Privacy">
-        <p className="mt-8 text-center text-base text-gray-400">&copy; Privacy</p></a>
+        <p className="mt-8 text-center text-base text-gray-400">&copy; Privacy</p>
+        </a>
         <p className="mt-8 text-center text-base text-gray-400">&copy; 2025 PodBubble. All rights reserved.</p>
       </div>
     </footer>
