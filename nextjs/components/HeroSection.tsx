04e4526e3f5552b6a6b7f7b5fb6dc4cfd5b21086
Rich Jones <103132290+richjones4364@users.noreply.github.com>
2025-03-10 22:36:50 +0000

    removed unused Link import

--- nextjs/components/HeroSection.tsx
+++ nextjs/components/HeroSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ChevronRight } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link'; // Correct import
 
 const HeroSection = () => {
   const scrollToLucy = () => {
